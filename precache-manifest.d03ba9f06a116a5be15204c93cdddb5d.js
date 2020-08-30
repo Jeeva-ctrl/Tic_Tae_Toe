2352620d826df8d9c5a864e9742cb58b1777b83d
Jeeva-ctrl <jeevabharathy66@gmail.com>
2020-08-30 17:00:54 +0530

    Updates

---
+++ precache-manifest.d03ba9f06a116a5be15204c93cdddb5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9477f64476f9864fc00e844ab098d3ff",
+    "url": "/Tic_Tae_Toe/index.html"
+  },
+  {
+    "revision": "28036d2c5465162923da",
+    "url": "/Tic_Tae_Toe/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9e8bb7e078af5fbc443a",
+    "url": "/Tic_Tae_Toe/static/css/main.ee984e4e.chunk.css"
+  },
+  {
+    "revision": "28036d2c5465162923da",
+    "url": "/Tic_Tae_Toe/static/js/2.473f63eb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Tic_Tae_Toe/static/js/2.473f63eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e8bb7e078af5fbc443a",
+    "url": "/Tic_Tae_Toe/static/js/main.a0bc97c7.chunk.js"
+  },
+  {
+    "revision": "8935f7a34b59adb6f394",
+    "url": "/Tic_Tae_Toe/static/js/runtime-main.fe956112.js"
+  }
+]);
